3b444b5c1d5ca8dc73f6193290a5b8ae0f24cde5
Megan Wachs <megan@sifive.com>
2017-04-12 10:28:21 -0700

    debug: Make SimJTAG a parameterized black box and increase default TCK frequency

--- src/main/scala/rocketchip/TestHarness.scala
+++ src/main/scala/rocketchip/TestHarness.scala
@@ -3,6 +3,8 @@
 package rocketchip
 
 import Chisel._
+// For parameterized black box
+import chisel3.experimental._
 import config._
 import junctions._
 import diplomacy._
@@ -27,7 +29,7 @@ class TestHarness()(implicit p: Parameters) extends Module {
   if (!p(IncludeJtagDTM)) {
     val dtm = Module(new SimDTM).connect(clock, reset, dut.io.debug.get, io.success)
   } else {
-    val jtag = Module(new SimJTAG).connect(dut.io.jtag.get, dut.io.jtag_reset.get, reset, io.success)
+    val jtag = Module(new SimJTAG(tickDelay = 2)).connect(dut.io.jtag.get, dut.io.jtag_reset.get, reset, io.success)
   }
 
   val mmio_sim = Module(LazyModule(new SimAXIMem(1, 4096)).module)
@@ -85,7 +87,7 @@ class SimDTM(implicit p: Parameters) extends BlackBox {
   }
 }
 
-class SimJTAG(implicit val p: Parameters) extends BlackBox {
+class SimJTAG(tickDelay: Int = 50) extends BlackBox(Map("TICK_DELAY" -> tickDelay)) {
   val io = new Bundle {
     val jtag = new JTAGIO(hasTRSTn = true)
     val enable = Bool(INPUT)
